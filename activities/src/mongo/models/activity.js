83d3e0788bd9f8021ad3a3794e443ecdafab3ce8
Federico Zolkwer <fedezolk@gmail.com>
2021-06-29 10:48:06 -0300

    Updated the kudos and comments schema

--- activities/src/mongo/models/activity.js
+++ activities/src/mongo/models/activity.js
@@ -12,8 +12,16 @@ const ACTIVITY_TYPE_DICTIONARY = {
 };
 
 const activitySchema = new Schema({
-  kudos_from__user: { type: [String], default: [] },
-  comments_from_users: { type: [String], default: [] },
+  kudos_from_users: { type: [String], default: [] },
+  comments_from_users: {
+    type: [
+      {
+        comment: String,
+        user_id: String,
+      },
+    ],
+    default: [],
+  },
   title: { type: String, required: true },
   user_id: { type: String, required: true },
   created_on: { type: Date, required: true },
